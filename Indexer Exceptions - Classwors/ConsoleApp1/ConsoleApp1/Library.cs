9863b9ea5e54f94eb194f44011e6ffdc5fae1628
Oruj Mehrabov <102570437+orucmehrabov@users.noreply.github.com>
2023-11-05 04:09:51 +0400

    First commit

---
+++ Indexer Exceptions - Classwors/ConsoleApp1/ConsoleApp1/Library.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Library
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public Book[] _books;
+        public Library(int size = 100)
+        {
+            _books = new Book[size];
+        }
+        public Book this[int index]
+        {
+            get
+            {
+                return _books[index];
+            }
+            set
+            {
+                _books[index] = value;
+            }
+        }
+    }
+}
---
+++ Indexer Exceptions - Classwors/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,78 @@
+﻿using ConsoleApp1;
+
+#region Indexer
+
+//Library library = new Library();
+//Book book = new Book();
+//book.Id = 1;
+//book.Name  = "Test1";
+
+//library[0] = book;
+
+//Console.WriteLine(library[0].Name);
+
+#endregion
+
+
+//try
+//{
+//	int[] nums = new int[0];
+//	nums[2] = 100;
+
+//	int a = 5;
+//	int b = 0;
+
+//	int result = a / b;
+
+//    Console.WriteLine(result);
+//}
+//catch (DivideByZeroException ex)
+//{
+//    Console.WriteLine(ex.Message);
+//}
+//catch (NullReferenceException ex)
+//{
+//    Console.WriteLine(ex.Message);
+//}
+//catch (IndexOutOfRangeException ex)
+//{
+//    Console.WriteLine(ex.Message);
+//}
+//finally
+//{
+//    Console.WriteLine("Mesaj yoxdur");
+//}
+
+
+static void Register(string username, string password);
+{
+    try
+    {
+        if (username == "oruc123")
+        {
+            Console.WriteLine("Register success");
+        }
+        else
+        {
+            throw new NullReferenceException("Register failed");
+        }
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine(ex.Message);
+    }
+}
+Register("oruc12", "ssfdhfh");
+
+
+static void SendEmail(bool isSuccess)
+{
+    if (isSuccess)
+    {
+        Console.WriteLine("success register email");
+    }
+    else
+    {
+        Console.WriteLine("Failed register email");
+    }
+}
